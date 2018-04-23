5e9532737facae9f0a93de6a542e27810eedf870
Kai Ransome <ransomekai@gmail.com>
2018-04-23 22:14:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,8 @@ def won?(board)
         return win_combination # return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
+    elsif board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        return false
     else
       false
     end
